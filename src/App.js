e0d07d361d77c56778cac186bb0c6b64af739727
Vladimir Tsiparsky <vladits@gmail.com>
2023-07-06 12:51:56 +0300

    Button, Panel and UserList components are adding

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import UsersList from "./components/UsersList";
 
 function App() {
   return (
-    <div>
-      Media Project
+    <div className="container mx-auto">
+      <UsersList />
     </div>
   );
 }
