79d7b091b54b5da096167b8d71b3e6d301e0f210
Vladimir Tsiparsky <vladits@gmail.com>
2023-07-07 11:01:50 +0300

    Rendering the list of users

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -23,9 +23,15 @@ function UsersList() {
         return <div>Error fetching data...</div>
     }
 
+    const renderedUsers = data.map((user) => {
+        return <div key={user.id} className="mb-2 border rounded">
+            <div className="flex p-2 justify-between items-center cursor-pointer">
+                {user.name}
+            </div>
+        </div>;
+    });
 
-
-    return <div>{data.length}</div>;
+    return <div>{renderedUsers}</div>;
 }
 
 export default UsersList;
