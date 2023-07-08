ff4f7c3d9b3b27f1189e7cd672ed168555e6c6a8
Vladimir Tsiparsky <vladits@gmail.com>
2023-07-08 20:34:33 +0300

    Creating a Fetch-Aware button component

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -43,11 +43,7 @@ function UsersList() {
     return <div>
         <div className="flex flex-row justify-between m-3">
             <h1 className="m-2 text-xl">Users List</h1>
-            {
-                isCreatingUser
-                    ? 'Creating User...'
-                    : <Button onClick={handleUserAdd}>+ Add User</Button>
-            }
+            <Button loading={isCreatingUser} onClick={handleUserAdd}>+ Add User</Button>
             {creatingUserError && 'Error creating user...'}
         </div>
         {renderedUsers}
