3ac998e0d51b9198b06dd929d1c882f0092e56f3
Vladimir Tsiparsky <vladits@gmail.com>
2023-07-14 19:51:52 +0300

    Section 22 Lesson 394

---
+++ src/store/apis/albumsApi.js
@@ -0,0 +1,44 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { faker } from '@faker-js/faker';
+
+const albumsApi = createApi({
+    reducerPath: 'albums',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'http://localhost:3005/',
+    }),
+    endpoints(builder) {
+        return {
+            fetchAlbums: builder.query({
+                providesTags: ['Album'],
+                query: (user) => {
+                    return {
+                        url: '/albums',
+                        params: {
+                            userId: user.id
+                        },
+                        method: 'GET'
+                    };
+                }
+            }),
+            addAlbum: builder.mutation({
+                invalidatesTags: ['Album'],
+                query: (user) => {
+                    return {
+                        url: '/albums',
+                        method: 'POST',
+                        body: {
+                            userId: user.id,
+                            title: faker.commerce.productName()
+                        }
+                    };
+                }
+            }) 
+        };
+    }
+});
+
+export const { 
+    useFetchAlbumsQuery, 
+    useAddAlbumMutation 
+} = albumsApi;
+export { albumsApi };
