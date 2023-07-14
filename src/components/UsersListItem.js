150a7e1eaaabe4d0e62040b967d4e0e2071ab579
Vladimir Tsiparsky <vladits@gmail.com>
2023-07-14 11:59:46 +0300

    Albom list component was added

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -2,23 +2,27 @@ import { GoTrash } from 'react-icons/go';
 import Button from './Button';
 import { removeUser } from '../store';
 import { useThunk } from '../hooks/use-thunk';
+import ExpandablePanel from './ExpadablePanel';
+import AlbumsList from './AlbumsList';
 
 function UsersListItem({ user}) {
     const [doRemoveUser, isLoading, error] = useThunk(removeUser);
 
     const handleTrashClick = () => doRemoveUser(user);
 
-    return <div className="mb-2 border rounded">
-                <div className="flex p-2 justify-between items-center cursor-pointer">
-                    <div className="flex flex-row items-center justify-between">
-                        <Button className="mr-3" loading={isLoading} onClick={handleTrashClick}>
-                            <GoTrash />
-                        </Button>
-                        { error && <div>Error deleting user.</div>}
-                        {user.name}
-                    </div>
-                </div>
-            </div>;
+    const header = <>
+        <Button className="mr-3" loading={isLoading} onClick={handleTrashClick}>
+            <GoTrash />
+        </Button>
+        { error && <div>Error deleting user.</div>}
+        {user.name}
+    </>;
+
+    return (
+        <ExpandablePanel header={header}>
+            <AlbumsList user={user}/>
+        </ExpandablePanel>
+    );
 }
 
 export default UsersListItem;
