ebae2dbb21ba4cafca83ee3d2296d798e9f8e919
Vladimir Tsiparsky <vladits@gmail.com>
2023-07-15 16:02:04 +0300

    Moer Clever Tag Implementation. Fix bag of albums loading

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -4,7 +4,7 @@ import Button from './Button';
 import AlbumListItem from "./AlbumsListItem";
 
 function AlbumsList({user}) {
-    const { data, error, isLoading } = useFetchAlbumsQuery(user);
+    const { data, error, isFetching } = useFetchAlbumsQuery(user);
     const [addAlbum, results] = useAddAlbumMutation();
 
     const handleAddAlbum = () => {
@@ -13,7 +13,7 @@ function AlbumsList({user}) {
 
     let content;
 
-    if (isLoading) {
+    if (isFetching) {
         content = <Skeleton className="h-10 w-full" times={3} />;
     } else if (error) {
         content = <div>Error loading albums.</div>
--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -22,7 +22,12 @@ const albumsApi = createApi({
         return {
             fetchAlbums: builder.query({
                 providesTags: (result, error, user) => {
-                    return [{ type: 'Album', id: user.id}];
+                    const tags = result.map(album => {
+                        return { type: 'Album', id: album.id };
+                    });
+                    tags.push({ type: 'UsersAlbums', id: user.id})
+
+                    return tags;
                 },
                 query: (user) => {
                     return {
@@ -36,7 +41,7 @@ const albumsApi = createApi({
             }),
             addAlbum: builder.mutation({
                 invalidatesTags: (result, error, user) => {
-                    return [{ type: 'Album', id: user.id }];
+                    return [{ type: 'UsersAlbums', id: user.id }];
                 },
                 query: (user) => {
                     return {
@@ -51,7 +56,7 @@ const albumsApi = createApi({
             }),
             removeAlbum: builder.mutation({
                 invalidatesTags: (result, error, album) => {
-                    return [{ type: 'Album', id: album.userId }];
+                    return [{ type: 'Album', id: album.id }];
                 },
                 query: (album) => {
                     return {
